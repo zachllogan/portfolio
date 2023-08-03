e30acaeece7fbf313b94d6ad27e8ed695ba60e2e
zachllogan <zachllogan@gmail.com>
2023-08-03 15:43:11 -0500

    Update theme

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -119,11 +119,11 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'wireframe',
+    defaultTheme: 'dracula',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
-    disableSwitch: false,
+    disableSwitch: true,
 
     // Should use the prefers-color-scheme media-query,
     // using user system preferences, instead of the hardcoded defaultTheme
