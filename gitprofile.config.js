85f0dc606e6e3068069aea11d00e31c200b1cbc3
Zach Logan <zachllogan@gmail.com>
2024-07-30 14:07:51 -0600

    Update gitprofile.config.js updated resume

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -30,7 +30,7 @@ const config = {
   },
   resume: {
     fileUrl:
-      'https://docs.google.com/document/d/1QrdmCTNjdF9NUc8lbc88X65RLlSejN2VBlz5Lyn153Y/edit?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
+      'https://docs.google.com/document/d/111FDOLoKPoFs7qfWZ1mbd5rTv5Hj6ioWpts2UeGelgc/edit?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'OOP',
